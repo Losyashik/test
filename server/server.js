10570cceec49ef22360c9f426fea3eae7e589b1b
Anton <An.Shkenin@yandex.ru>
2022-12-03 09:04:24 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,8 +8,11 @@ const PORT = 3000;
 const app = express();
 app.use(express.json());
 app.use(express.static("dist"));
+app.use("/SummaryTable", express.static("dist"));
 app.use("/api/users", userRouter);
 app.use("/api/docs", docRouter);
+app.use("/SummaryTable/api/users", userRouter);
+app.use("/SummaryTable/api/docs", docRouter);
 
 function startServe() {
   try {
